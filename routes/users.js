5f2ba3dee44eabd331a22dad936749c22ee7b228
superphung <superphung44@gmail.com>
2015-03-21 23:55:39 +0800

    git added user endpoint mysql

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,64 @@
 var express = require('express');
-var middleware = require('../middlewares/users_middlewares');
 var utilities = require('../middlewares/utilities_module');
+var middleware;
 
-var router = express.Router();
+module.exports = function(db) {
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+    if (db === 'mongodb') {
+        middleware = require('../middlewares/users_middlewares');
+        console.log('use mongodb users middleware');
+    }
+    else if (db === 'mysql') {
+        middleware = require('../middlewares/users_middlewares_mysql');
+        console.log('use mysql users middleware');
+    }
 
-router.post('/user', function(req, res, next) {
-    utilities.useMiddleware(middleware.create, req, res, next);
-});
+    var router = express.Router();
 
-router.get('/user', function(req, res, next) {
-    utilities.useMiddleware(middleware.getAll, req, res, next);
-});
+    /* GET users listing. */
+    router.get('/', function (req, res, next) {
+        res.send('respond with a resource');
+    });
 
-router.get('/user/:id', function(req, res, next) {
-    utilities.useMiddleware(middleware.getById, req, res, next);
-});
+    router.post('/user', function (req, res, next) {
+        utilities.useMiddleware(middleware.create, req, res, next);
+    });
 
-router.get('/user/:id/creation', function(req, res, next) {
-    utilities.useMiddleware(middleware.getCreation, req, res, next);
-});
+    router.get('/user', function (req, res, next) {
+        utilities.useMiddleware(middleware.getAll, req, res, next);
+    });
 
-router.get('/user/:id/group', function(req, res, next) {
-    utilities.useMiddleware(middleware.getGroup, req, res, next);
-});
+    router.get('/user/:id', function (req, res, next) {
+        utilities.useMiddleware(middleware.getById, req, res, next);
+    });
 
-router.get('/user/:id/likes', function(req, res, next) {
-    utilities.useMiddleware(middleware.getLikes, req, res, next);
-});
+    router.get('/user/:id/creation', function (req, res, next) {
+        utilities.useMiddleware(middleware.getCreation, req, res, next);
+    });
 
-router.get('/user/:id/comments', function(req, res, next) {
-    utilities.useMiddleware(middleware.getComments, req, res, next);
-});
+    router.get('/user/:id/group', function (req, res, next) {
+        utilities.useMiddleware(middleware.getGroup, req, res, next);
+    });
 
-router.get('/user/:id/friends', function(req, res, next) {
-    utilities.useMiddleware(middleware.getFriends, req, res, next);
-});
+    router.get('/user/:id/likes', function (req, res, next) {
+        utilities.useMiddleware(middleware.getLikes, req, res, next);
+    });
 
-router.put('/user/:id', function(req, res, next) {
-    utilities.useMiddleware(middleware.update, req, res, next);
-});
+    router.get('/user/:id/comments', function (req, res, next) {
+        utilities.useMiddleware(middleware.getComments, req, res, next);
+    });
 
-router.delete('/user/:id', function(req, res, next) {
-    utilities.useMiddleware(middleware.delete, req, res, next);
-});
+    router.get('/user/:id/friends', function (req, res, next) {
+        utilities.useMiddleware(middleware.getFriends, req, res, next);
+    });
 
-module.exports = router;
+    router.put('/user/:id', function (req, res, next) {
+        utilities.useMiddleware(middleware.update, req, res, next);
+    });
+
+    router.delete('/user/:id', function (req, res, next) {
+        utilities.useMiddleware(middleware.delete, req, res, next);
+    });
+
+    return router;
+};
