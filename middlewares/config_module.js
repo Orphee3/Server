0e81d8820ce084e55143176653e6f548338f7d2c
superphung <superphung44@gmail.com>
2015-07-03 01:26:44 +0800

    added secret for express jwt middleware

--- middlewares/config_module.js
+++ middlewares/config_module.js
@@ -4,8 +4,8 @@
 
 module.exports = function(nconf) {
     nconf.defaults({
-        //'db': 'mongodb',
-        'db': 'mysql',
+        'db': 'mongodb',
+        //'db': 'mysql',
         'secret': 'superphung'
     });
 };
