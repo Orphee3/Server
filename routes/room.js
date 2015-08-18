54a105fe4959b521596cbafe04080e590ca2ce64
superphung <superphung44@gmail.com>
2015-08-19 01:00:07 +0200

    added enpoints for room, added get private message middleware

---
+++ routes/room.js
@@ -0,0 +1,22 @@
+/**
+ * Created by superphung on 8/19/15.
+ */
+var express = require('express');
+var utilities = require('../middlewares/utilities_module');
+var nconf = require('nconf');
+var authorization = require('../middlewares/authorization_module');
+var middleware;
+
+if (nconf.get('db') === mongodb) {
+    middleware = require('../middlewares/room_middlewares');
+}
+
+var router = express.Router();
+
+router.get('/room/privateMessage/:id',
+    authorization.validateToken({secret: nconf.get('secret')}),
+    function (req, res, next) {
+        utilities.useMiddleware(middleware.getPrivateMessage, req, res, next);
+    });
+
+module.exports = router;
